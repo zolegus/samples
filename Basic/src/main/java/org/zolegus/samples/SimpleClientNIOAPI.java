8ec4afd910e368e3f3b333b136ed163f69ed12f6
Oleg Zherebkin <zolegus@gmail.com>
2014-10-16 18:32:35 +0400

    simple client NIO

---
+++ Basic/src/main/java/org/zolegus/samples/SimpleClientNIOAPI.java
@@ -0,0 +1,72 @@
+package org.zolegus.samples;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public class SimpleClientNIOAPI implements Runnable{
+
+    private SocketChannel socketChannel = null;
+    private ByteBuffer in = ByteBuffer.allocate(64);
+    private ByteBuffer out = ByteBuffer.allocate(64);
+    private boolean isConnected = false;
+
+    public SimpleClientNIOAPI(SocketAddress address) {
+
+        try {
+            socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+            socketChannel.connect(address);
+            while( !socketChannel.finishConnect() ){}
+            isConnected = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void disconnect() {
+        try {
+            socketChannel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        if (!isConnected)
+            return;
+        try {
+            int bytesRead = socketChannel.read(in);
+            while (bytesRead != -1) {
+                in.flip();
+                while (in.remaining() > 0)
+                    System.out.print((char) in.get());
+
+                in.clear();
+                bytesRead = socketChannel.read(in);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Client connection closed.");
+        isConnected = false;
+    }
+
+
+    public void sendToServer(char message) throws Exception {
+        if (!isConnected)
+            throw new Exception("Socket channel is not connected");
+        out.clear();
+        out.put((byte)message);
+        out.flip();
+        while(out.hasRemaining()) {
+            socketChannel.write(out);
+        }
+    }
+
+}
