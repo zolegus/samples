36380abcbc26613641d88e503c3ddc116087e395
Oleg Zherebkin <zolegus@gmail.com>
2014-11-17 22:52:37 +0300

    performance test

--- Basic/src/main/java/org/zolegus/samples/chronicle/SimpleObject.java
+++ Basic/src/main/java/org/zolegus/samples/chronicle/SimpleObject.java
@@ -8,6 +8,7 @@ import net.openhft.chronicle.IndexedChronicle;
 import net.openhft.chronicle.tools.ChronicleTools;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Random;
 
 /**
@@ -15,35 +16,38 @@ import java.util.Random;
  */
 public class SimpleObject {
 
-    public static void main(String[] args) throws IOException {
-        String basePath = System.getProperty("java.io.tmpdir") + "/SimpleChronicle";
-        ChronicleTools.deleteOnExit(basePath);
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String basePath = System.getProperty("java.io.tmpdir") + "/data";
+        //ChronicleTools.deleteOnExit(basePath);
 
         IndexedChronicle chronicle = new IndexedChronicle(basePath);
         ExcerptAppender appender = chronicle.createAppender();
+
+
         // write one object
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-
-                    for (int i = 1; i <= 100000000; i++) {
+                    int count = 10000000;
+                    appender.toEnd();
+                    System.out.println("The writer started");
+                    for (int i = 1; i <= count; i++) {
                         appender.startExcerpt(); // an upper limit to how much space in bytes this message should need.
-                        appender.writeObject(i);
-                        appender.writeObject(i);
-                        appender.writeObject(i);
-                        appender.writeObject(i);
-                        appender.writeObject(i);
+                        appender.writeLong(System.currentTimeMillis());
                         appender.finish();
                     }
                 } catch (Exception e) {
                     throw new AssertionError(e);
                 }
                 finally {
-                    System.out.println("Writer finished!");
+                    System.out.println("Writer finished");
                 }
             }
         });
+
+
+//        thread.start();
 
         // read one object
         ExcerptTailer reader = chronicle.createTailer();
@@ -51,33 +55,26 @@ public class SimpleObject {
         reader.toStart();
         boolean startT = false;
         boolean stop = false;
-        int i = 0;
-        Object ret;
-        while(!stop) {
+        int count = 0;
+        long ret;
+//        while(!stop) {
+        long start = System.nanoTime();
             while (reader.nextIndex()) {
-                ret = reader.readObject();
-                ret = reader.readObject();
-                ret = reader.readObject();
-                ret = reader.readObject();
-                ret = reader.readObject();
+                ret = reader.readLong();
                 reader.finish();
+                count++;
 //                System.out.println(ret);
-                if (++i == 100000000) {
-                    stop = true;
-                    break;
+//                if ( == 1000) {
+//                    stop = true;
+//                    break;
                 }
-            }
+//            }
+//        }
 
-            if (!startT) {
-                System.out.println("Start writer!");
-                thread.start();
-                startT = true;
-            }
-
-
-
-        }
 //        reader.free();
-        System.out.println("The end!");
+        long time = System.nanoTime() - start;
+        System.out.printf("Average read time was %.2f us %n", time / count / 1e3);
+        System.out.println("The end. There was read " + count);
+//        thread.join();
     }
 }
